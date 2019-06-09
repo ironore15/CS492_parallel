1312db7f7cb3f887053e7ecfa9f2344d47b56997
ironore15 <alex8757@kaist.ac.kr>
2019-06-09 10:31:02 +0000

    Implement test version for Problem2

--- HW3/Problem2/prob2.cu
+++ HW3/Problem2/prob2.cu
@@ -8,14 +8,45 @@
 #define NANO 1000000000
 #define PGSIZE 0x1000
 #define SUBSIZE 1000
-#define BLOCK 128
-#define GRID 8
+// #define BLOCK 128
+#define GRID 32
 
 int size;
-float *matrixA, *matrixB, *matrixBT, *matrixC_serial, *matrixC_cuda;
+float *matrixA, *vectorB, *vectorX;
 
 __global__
-void cudaMatMul(float *A_d, float *B_d, float *C_d, int x, int y, int n, int size)
+void cudaPivot(float *A_d, int *max_pivot, int pivot, int size)
+{
+    float max = A_d[pivot * size + pivot];
+    int maxi = pivot;
+    for (int i = pivot + 1; pivot < size; pivot++)
+    {
+        float abs = fabsf(A_d[i * size + pivot]);
+        if (abs > max)
+        {
+            max = abs;
+            maxi = i;
+        }
+    }
+    *max_pivot = maxi;
+    return;
+}
+
+__global__
+void cudaSwap(float *A_d, int *max_pivot, int pivot, int size)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pivot <= i && i < size)
+    {
+        float temp = A_d[pivot * size + i];
+        A_d[pivot * size + i] = A_d[*max_pivot * size + i];
+        A_d[*max_pivot * size + i] = temp;
+    }
+    return;
+}
+
+__global__
+void cudaGauss(float *A_d, float *B_d, float *C_d, int x, int y, int n, int size)
 {
     int i = n * x + blockIdx.x * blockDim.x + threadIdx.x;
     int j = n * y + blockIdx.y * blockDim.y + threadIdx.y;
@@ -30,6 +61,24 @@ void cudaMatMul(float *A_d, float *B_d, float *C_d, int x, int y, int n, int siz
     C_d[i * size + j] = value;
     return;
 }
+
+float *make_vector(int size)
+{
+    float *vector = (float *) malloc(sizeof(float) * size);
+    if (vector == NULL)
+    {
+        perror("malloc");
+        exit(0);
+    }
+
+    if (malloc(PGSIZE) == NULL)
+    {
+        perror("malloc");
+        exit(0);
+    }
+    return vector;
+}
+
 
 float *make_matrix(int size)
 {
@@ -46,6 +95,12 @@ float *make_matrix(int size)
         exit(0);
     }
     return matrix;
+}
+
+void set_vector(float *vector, int size)
+{
+    for (int i = 0; i < size; i++)
+        vector[i] = (float) drand48();
 }
 
 void set_matrix(float *matrix, int size)
@@ -69,45 +124,48 @@ void print_matrix(double *matrix, int size)
     printf(" ]\n");
 }
 
-void cuda_mmul(float *A, float *B, float *C, int size)
+void cuda_gaussian(float *A, float *B, float *X, int size)
 {
-    int mem_size = sizeof(float) * size * size;
-    float *A_d, *B_d, *C_d;
-    dim3 dimBlock(BLOCK, BLOCK);
+    int mat_size = sizeof(float) * size * size;
+    int vec_size = sizeof(float) * size;
+    int block_size = (size / GRID) + (size % GRID == 0) ? 0 : 1;
+    int *max_pivot;
+    float *A_d, *B_d;
+    dim3 dimBlock(block_size, block_size);
     dim3 dimGrid(GRID, GRID);
 
-    cudaMalloc((void **) &A_d, mem_size);
-    cudaMemcpy(A_d, A, mem_size, cudaMemcpyHostToDevice);
-    cudaMalloc((void **) &B_d, mem_size);
-    cudaMemcpy(B_d, B, mem_size, cudaMemcpyHostToDevice);
+    cudaMalloc((void **) &A_d, mat_size);
+    cudaMemcpy(A_d, A, mat_size, cudaMemcpyHostToDevice);
+    cudaMalloc((void **) &B_d, vec_size);
+    cudaMemcpy(B_d, B, vec_size, cudaMemcpyHostToDevice);
+    cudaMalloc((void **) &max_pivot, sizeof(int));
 
-    cudaMalloc((void **) &C_d, mem_size);
-
-    for (int i = 0; i < size / SUBSIZE; i++)
+    for (int pivot = 0; pivot < size; pivot++)
     {
-        for (int j = 0; j < size / SUBSIZE; j++)
-            cudaMatMul<<<dimBlock, dimGrid>>>(A_d, B_d, C_d, i, j, SUBSIZE, size);
+        cudaPivot<<<1, 1>>>(A_d, max_pivot, pivot, size);
+        cudaSwap<<<1, size>>>(A_d, max_pivot, pivot, size);
     }
 
-    cudaMemcpy(C, C_d, mem_size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(X, B_d, vec_size, cudaMemcpyDeviceToHost);
 
     cudaFree(A_d);
     cudaFree(B_d);
-    cudaFree(C_d);
 }
 
-void serial_mmul(float *matrixA, float *matrixB, float *matrixC)
+float L2_norm(float *matrixA, float *vectorX, float *vectorB)
 {
+    float norm = 0.0, sum;
     for (int i = 0; i < size; i++)
     {
+        sum = 0.0;
         for (int j = 0; j < size; j++)
-        {
-            matrixC[i * size + j] = 0.0;
-            for (int k = 0; k < size; k++)
-                matrixC[i * size + j] += matrixA[i * size + k] * matrixB[k * size + j];
-        }
+            sum += matrixA[i * size + j] * vectorX[j];
+        sum -= vectorB[i];
+        norm += sum * sum;
     }
+    return sqrt(norm);
 }
+
 
 int main(int argc, char **argv, char **envp)
 {
@@ -134,13 +192,12 @@ int main(int argc, char **argv, char **envp)
     }
 
     matrixA = make_matrix(size);
-    matrixB = make_matrix(size);
-    matrixC_serial = make_matrix(size);
-    matrixC_cuda = make_matrix(size);
+    vectorB = make_vector(size);
+    vectorX = make_vector(size);
 
     srand48(time(NULL));
     set_matrix(matrixA, size);
-    set_matrix(matrixB, size);
+    set_vector(vectorB, size);
 
     printf("Multi Thread Computation Start\n");
 
@@ -150,7 +207,7 @@ int main(int argc, char **argv, char **envp)
         exit(0);
     }
 
-    cuda_mmul(matrixA, matrixB, matrixC_cuda, size);
+    cuda_gaussian(matrixA, vectorB, vectorX, size);
 
     if (clock_gettime(CLOCK_MONOTONIC, &tend) == -1)
     {
@@ -164,45 +221,8 @@ int main(int argc, char **argv, char **envp)
 
     printf("Multi Thread Computation End: %.3f us.\n", microsec);
 
-    printf("Single Thread Computation Start\n");
 
-    if (clock_gettime(CLOCK_MONOTONIC, &tstart) == -1)
-    {
-        perror("clock_gettime");
-        exit(0);
-    }
-
-    serial_mmul(matrixA, matrixB, matrixC_serial);
-
-    if (clock_gettime(CLOCK_MONOTONIC, &tend) == -1)
-    {
-        perror("clock_gettime");
-        exit(0);
-    }
-
-    start_nsec = tstart.tv_sec * NANO + tstart.tv_nsec;
-    end_nsec = tend.tv_sec * NANO + tend.tv_nsec;
-    microsec = (end_nsec - start_nsec) / 1000.0;
-
-    printf("Single Thread Computation End: %.3f us.\n", microsec);
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (fabs(matrixC_cuda[i * size + j] - matrixC_serial[i * size + j]) > 1e-3)
-            {
-                printf("Verification Fail.\n");
-                printf("(%d, %d): %f - %f\n", i, j, matrixC_cuda[i * size + j], matrixC_serial[i * size + j]);
-            }
-        }
-    }
-    printf("Verification Success.\n");
-
-
-    // print_matrix(matrixA, size);
-    // print_matrix(matrixB, size);
-    // print_matrix(matrixC, size);
+    printf("L2-norm : %g\n", L2_norm(matrixA, vectorX, vectorB));
 
     return 0;
 }
